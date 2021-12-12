2323dbd010c1ed0cdc9e01d168c83a5c87355d53
dev420 <sspike191919@gmail.com>
2021-12-12 23:02:36 +0300

    3d interactive game map complete

---
+++ precache-manifest.9834594445de7b6200ce260262c38b20.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc061d6d94f29a30c6f4b2e8d02c72d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78e9e6e3299db78c3489",
+    "url": "./static/css/main.de49b600.chunk.css"
+  },
+  {
+    "revision": "e212d7e281e8347ffc7e",
+    "url": "./static/js/2.b9fcb02c.chunk.js"
+  },
+  {
+    "revision": "5f81535fab8ca9a38aadb008c3904b06",
+    "url": "./static/js/2.b9fcb02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e9e6e3299db78c3489",
+    "url": "./static/js/main.3e95b0a0.chunk.js"
+  },
+  {
+    "revision": "ebf8d0c7ddcddf4b0f33",
+    "url": "./static/js/runtime-main.a8dbe91c.js"
+  },
+  {
+    "revision": "fa23950021b73df751065d9fcb2e3562",
+    "url": "./static/media/ADSK-BIM-Collaborate.fa239500.png"
+  },
+  {
+    "revision": "6759bdf9dccd5e5e5cec5ea0fbfa7e02",
+    "url": "./static/media/ADSK-Build.6759bdf9.png"
+  },
+  {
+    "revision": "ea15f3d47c3b16ede177bbecf48dee32",
+    "url": "./static/media/ADSK-Docs.ea15f3d4.png"
+  },
+  {
+    "revision": "59af85c0c5af71a46c3b8bbf08cda8a5",
+    "url": "./static/media/ADSK-Quantify.59af85c0.png"
+  },
+  {
+    "revision": "7489bf556f8b5deb5f5de23a15b3d211",
+    "url": "./static/media/AMERACAS.7489bf55.fbx"
+  },
+  {
+    "revision": "3cab302188229dd2830ce50b13b83937",
+    "url": "./static/media/APAC.3cab3021.fbx"
+  },
+  {
+    "revision": "2ab02c2937018ee7f2ccc346929ccf57",
+    "url": "./static/media/Artifakt.2ab02c29.ttf"
+  },
+  {
+    "revision": "0cec6258fa45a6713cfbc64afafb15c6",
+    "url": "./static/media/Artifakt_Bold.0cec6258.otf"
+  },
+  {
+    "revision": "9a80b3ecfff5633f8ea5a2461ad90b6a",
+    "url": "./static/media/Assemble.9a80b3ec.png"
+  },
+  {
+    "revision": "d4d7953d70144d0e174403c9f22431f2",
+    "url": "./static/media/BuildingConnected.d4d7953d.png"
+  },
+  {
+    "revision": "8c8905b9e5a727f74debd5a1519544fd",
+    "url": "./static/media/Calibri_Bold.8c8905b9.TTF"
+  },
+  {
+    "revision": "0d717c4bf80cbb556bf87777fef7d09e",
+    "url": "./static/media/Calibri_Light.0d717c4b.ttf"
+  },
+  {
+    "revision": "8de6a7bf208380da251e9889b139afef",
+    "url": "./static/media/EMEA_custom.8de6a7bf.fbx"
+  },
+  {
+    "revision": "0d30313c5f4a32b9a188e35f8c8c4d53",
+    "url": "./static/media/bridge.0d30313c.fbx"
+  },
+  {
+    "revision": "fa75ffd9e9755e6763e8b4ffd95b76d2",
+    "url": "./static/media/btn_leaderboard.fa75ffd9.svg"
+  },
+  {
+    "revision": "6a19fb1d69b4df8076c628de6e320244",
+    "url": "./static/media/btn_next.6a19fb1d.svg"
+  },
+  {
+    "revision": "cb3632ab7f3779257bf2e36ef79d65a6",
+    "url": "./static/media/btn_play.cb3632ab.svg"
+  },
+  {
+    "revision": "c94db4304fe061e9593399b1e74fcff2",
+    "url": "./static/media/btn_pre.c94db430.svg"
+  },
+  {
+    "revision": "477a316c883761270baa5bed82abd7a3",
+    "url": "./static/media/btn_rules.477a316c.svg"
+  },
+  {
+    "revision": "758895f8cf224d6264743108ae2ded12",
+    "url": "./static/media/building.758895f8.fbx"
+  },
+  {
+    "revision": "6e67bb0baff934accc4ba0dd5385c00d",
+    "url": "./static/media/crane.6e67bb0b.fbx"
+  },
+  {
+    "revision": "3cbfeb82e02b58c80706457ad140ceaf",
+    "url": "./static/media/game_difficult.3cbfeb82.png"
+  },
+  {
+    "revision": "4b30052f291d783d54cca5c2ad5a35f0",
+    "url": "./static/media/game_easy.4b30052f.png"
+  },
+  {
+    "revision": "650e72631ff3b3409a6fa91a8be9baff",
+    "url": "./static/media/game_medium.650e7263.png"
+  },
+  {
+    "revision": "e8d1afff4195958e1960b39689b06f46",
+    "url": "./static/media/game_modal_leader.e8d1afff.svg"
+  },
+  {
+    "revision": "a84a8e19163fb970f4cf27a3a6deeefe",
+    "url": "./static/media/game_modal_play.a84a8e19.svg"
+  },
+  {
+    "revision": "77bf06a7540dc70227c3d0fd8d8368bc",
+    "url": "./static/media/game_modal_rule.77bf06a7.svg"
+  },
+  {
+    "revision": "7bf87c9605a83978658507f54cc80368",
+    "url": "./static/media/island_game.7bf87c96.fbx"
+  },
+  {
+    "revision": "eb9dcbf46bc91b0d9b40908ba9ef137f",
+    "url": "./static/media/island_media.eb9dcbf4.fbx"
+  },
+  {
+    "revision": "d60c32d0f62f816c78d5d5afbc9fb5a0",
+    "url": "./static/media/logo.d60c32d0.png"
+  },
+  {
+    "revision": "8b2c50d818e38f3358905032bc0862f9",
+    "url": "./static/media/logo_mobile.8b2c50d8.png"
+  },
+  {
+    "revision": "f4317f26f3b68c5e718c26d6814a3a7a",
+    "url": "./static/media/main-logo.f4317f26.svg"
+  },
+  {
+    "revision": "f91958fa72f1342df3c5ab53c330df99",
+    "url": "./static/media/menu_conductive.f91958fa.svg"
+  },
+  {
+    "revision": "12f936dc64f9debda8e5950b719eef1b",
+    "url": "./static/media/menu_game.12f936dc.svg"
+  },
+  {
+    "revision": "7748e4757ce86512f5c2d813e6cd5211",
+    "url": "./static/media/menu_home_0.7748e475.svg"
+  },
+  {
+    "revision": "d595d58a2f4027ea1124632923bcc3c6",
+    "url": "./static/media/menu_home_1.d595d58a.svg"
+  },
+  {
+    "revision": "2ab2af3100994c2c55be5aca16190b8d",
+    "url": "./static/media/menu_home_2.2ab2af31.svg"
+  },
+  {
+    "revision": "48db3c0a8a2e2836d32c051756abcb37",
+    "url": "./static/media/menu_map.48db3c0a.svg"
+  },
+  {
+    "revision": "78d841a82686a4a6b509b1a5f8a0c360",
+    "url": "./static/media/menu_media.78d841a8.svg"
+  },
+  {
+    "revision": "bc92b3f6dd50260f10c6ffde09e75da5",
+    "url": "./static/media/redo.bc92b3f6.png"
+  },
+  {
+    "revision": "d4d7953d70144d0e174403c9f22431f2",
+    "url": "./static/media/result_building.d4d7953d.png"
+  },
+  {
+    "revision": "6916b183f69fdb74269d527121ebdb83",
+    "url": "./static/media/stadium.6916b183.fbx"
+  },
+  {
+    "revision": "dd8515b8343e8d84652347d8ad9fa03e",
+    "url": "./static/media/trans_move.dd8515b8.svg"
+  },
+  {
+    "revision": "a41c3cef977f46ef25dc4b91c724c87f",
+    "url": "./static/media/trans_rotate.a41c3cef.svg"
+  },
+  {
+    "revision": "dd8952b07fdb1d7d74da72fa301e0113",
+    "url": "./static/media/undo.dd8952b0.png"
+  }
+]);
